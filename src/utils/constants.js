ee131e51b5c02548e682c3312b24d593e243ac27
Ekam <bhat0155@algonauinlive.com>
2025-05-09 18:46:41 -0400

    base url changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL="/api";
+// export const BASE_URL="/api"; //production
+export const BASE_URL= location.hostname === "localhost" ? "http://localhost:3000" : "/api"
 
 // changed url for backend
 // this will be reflected in network calls
