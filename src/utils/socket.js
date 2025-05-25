d3906eca913e4e3f6f9ed9eb59715df455f996d5
Ekam <bhat0155@algonauinlive.com>
2025-05-25 13:37:25 -0400

    chat io changes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,8 +1,12 @@
 import { BASE_URL } from "./constants";
-import io from "socket.io-client"
+import io from "socket.io-client";
 
-export const createSocketConnection = () =>{
-    return io(BASE_URL)
-}
+export const createSocketConnection = () => {
+  if (location.hostname === "localhost") {
+    return io(BASE_URL);
+  } else {
+    return io("/" + { path: "/api/socket.io" });
+  }
+};
 
 export default createSocketConnection;
